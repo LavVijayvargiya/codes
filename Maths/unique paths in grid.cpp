65195c322c8269e0784ad610c75e92f2aa4b66c0
LavVijayvargiya <lav.vijay14@gmail.com>
2018-05-30 01:20:57 +0530

    new

--- Maths/unique paths in grid.cpp
+++ Maths/unique paths in grid.cpp
@@ -1,4 +1,8 @@
 int Solution::uniquePaths(int A, int B) {
+	//the logic is that you have to traverse total
+	// m-1 + n-1 steps, so choose out of these steps
+	//when you want to take down or right.
+	// C(m+n-2,n-1)
     long long ans = 1;
     for (int i = B; i < (A + B - 1); i++) {
         ans *= i;
